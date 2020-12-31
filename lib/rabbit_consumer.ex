33955423e4caa59764e632eb79a7b2f1a47b4b28
Dave Wu <eyaswoo@gmail.com>
2020-12-31 22:56:42 +0800

    wait for testing

--- lib/rabbit_consumer.ex
+++ lib/rabbit_consumer.ex
@@ -24,20 +24,11 @@ defmodule RabbitConsumer do
       @binding_keys unquote(binding_keys)
 
       use GenServer
+      require Logger
 
-      def on_ready() do
-      end
-
-      def on_ending() do
-      end
-
-      defp wait_for_msg() do
-        receive do
-          {_deliver, payload, meta} ->
-            apply(@me, @proc_fn, payload: payload, meta: meta)
-            wait_for_msg()
-        end
-      end
+      def on_ready(), do: IO.puts("#{@me} as RabbitConsumer is ready.")
+      def on_ending(), do: IO.puts("#{@me} as RabbitConsumer is leaving.")
+      defoverridable(on_ready: 0, on_ending: 0)
 
       def start_link(opts) do
         opts = Keyword.put(opts, :name, @me)
@@ -46,28 +37,38 @@ defmodule RabbitConsumer do
 
       @impl GenServer
       def init(_) do
-        {:ok, conn} = AMQP.Connection.open(host: @addr)
-        {:ok, chan} = AMQP.Channel.open(conn)
-        AMQP.Exchange.declare(chan, @ex, @ex_type)
-        {:ok, %{queue: queue_name}} = AMQP.Queue.declare(chan, @q, @q_opts)
+        case AMQP.Connection.open(host: @addr) do
+          {:ok, conn} ->
+            {:ok, chan} = AMQP.Channel.open(conn)
+            AMQP.Exchange.declare(chan, @ex, @ex_type)
+            {:ok, %{queue: queue_name}} = AMQP.Queue.declare(chan, @q, @q_opts)
 
-        for binding_key <- @binding_keys do
-          AMQP.Queue.bind(chan, queue_name, @ex, routing_key: binding_key)
+            for binding_key <- @binding_keys do
+              AMQP.Queue.bind(chan, queue_name, @ex, routing_key: binding_key)
+            end
+
+            AMQP.Basic.consume(chan, queue_name, nil, no_ack: true)
+            apply(@me, :on_ready)
+            wait_for_msg()
+            apply(@me, :on_ending)
+            AMQP.Connection.close(conn)
+            {:ok, []}
+
+          {:error, reason} ->
+            Logger.error("Failed to connect to Rabbit: #{reason}")
+            {:stop, reason}
+
+          _ ->
+            {:stop, "Unknown Error"}
         end
-
-        AMQP.Basic.consume(chan, queue_name, nil, no_ack: true)
-
-        apply(@me, :on_ready)
-        wait_for_msg()
-        apply(@me, :on_ending)
-
-        AMQP.Connection.close(conn)
-        {:ok, []}
       end
 
-      @impl GenServer
-      def handle_info(call, msg) do
-        {:noreply, []}
+      defp wait_for_msg() do
+        receive do
+          {_deliver, payload, meta} ->
+            apply(@me, @proc_fn, payload: payload, meta: meta)
+            wait_for_msg()
+        end
       end
     end
   end
--- test/rabbit_consumer_test.exs
+++ test/rabbit_consumer_test.exs
@@ -1,14 +1,6 @@
 defmodule MyConsumer do
   use RabbitConsumer
 
-  def on_ready() do
-    IO.puts("I am ready")
-  end
-
-  def on_ending() do
-    IO.puts("I am leaving")
-  end
-
   def process(payload: payload, meta: _meta) do
     IO.puts("payload=#{payload}")
   end
